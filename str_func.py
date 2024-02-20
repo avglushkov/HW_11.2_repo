d9cb32206337acef8014354df51274aab9eb03b1
Glushkov Alex <avgl@mail.ru>
2024-02-20 15:15:28 +0300

    hotfix imitation

---
+++ str_func.py
@@ -0,0 +1,11 @@
+def text_up(text):
+    """Функция преобразования тектста строки в верхний регистр"""
+
+    return text.upper()
+
+def first_up(text):
+    """Функция перевода первого символа в строке в верхний регистр"""
+
+    return text.capitalize()
+
+print("hotfix")
